824615e37af6a47b7ff654846899d488a3b82177
Omar Ahmed <44481508+aamosa@users.noreply.github.com>
2019-03-03 21:32:48 -0600

    Update beansUtil.java

--- beansUtil.java
+++ beansUtil.java
@@ -1,9 +1,9 @@
-	public List<String> getBeanList(String beanName) {
-		List<String> beanList = new ArrayList<>();
-		BeanManager bm = CDI.current().getBeanManager();
-		Iterator it = bm.getBeans(beanName).iterator();
-		while(it.hasNext()) {
-			 beanList.add(it.next().toString());
-		}
-		return beanList;
+public List<String> getBeanList(String beanName) {
+	List<String> beanList = new ArrayList<>();
+	BeanManager bm = CDI.current().getBeanManager();
+	Iterator it = bm.getBeans(beanName).iterator();
+	while(it.hasNext()) {
+		 beanList.add(it.next().toString());
 	}
+	return beanList;
+}
