368796566c7b7a86c37c73f4e0a40cab58005dcc
Omar Ahmed <44481508+aamosa@users.noreply.github.com>
2019-03-06 10:16:37 -0600

    Update beansUtil.java

--- beansUtil.java
+++ beansUtil.java
@@ -1,9 +1,13 @@
-public List<String> getBeanList(String beanName) {
-	List<String> beanList = new ArrayList<>();
-	BeanManager bm = CDI.current().getBeanManager();
-	Iterator it = bm.getBeans(beanName).iterator();
-	while(it.hasNext()) {
-		 beanList.add(it.next().toString());
-	}
-	return beanList;
+/**
+ * Utility method for getting list of CDI managed beans by name.
+ * Beans will have to override toString method for any meaningful information.
+ */
+public static List<String> getBeanList(String beanName) {
+   List<String> beanList = new ArrayList<>();
+   BeanManager bm = CDI.current().getBeanManager();
+   Iterator it = bm.getBeans(beanName).iterator();
+   while(it.hasNext()) {
+       beanList.add(it.next().toString());
+   }
+   return beanList;
 }
